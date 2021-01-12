c1dfe6cbddb0ad1bbd5c2da7cbc9cd7385e21fb0
hongjinseok <sldkdl111@ajou.ac.kr>
2021-01-12 17:18:34 +0900

    MySelectShop

---
+++ src/main/java/com/sparta/week04/controller/SearchRequestController.java
@@ -0,0 +1,24 @@
+package com.sparta.week04.controller;
+
+import com.sparta.week04.models.ItemDto;
+import com.sparta.week04.utils.NaverShopSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor // final 로 선언된 클래스를 자동으로 생성
+@RestController // JSON으로 응답함을 선언
+
+public class SearchRequestController {
+
+    private final NaverShopSearch naverShopSearch;
+
+    @GetMapping("/api/search")
+    public List<ItemDto> getItems(@RequestParam String query) {
+        String result = naverShopSearch.search(query);
+        return naverShopSearch.fromJSONtoItems(result);
+    }
+}
--- src/main/java/com/sparta/week04/utils/NaverShopSearch.java
+++ src/main/java/com/sparta/week04/utils/NaverShopSearch.java
@@ -1,21 +1,31 @@
 package com.sparta.week04.utils;
 
+import com.sparta.week04.models.ItemDto;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.http.*;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
+@Component
 public class NaverShopSearch {
-    public String search() {
+
+    public String search(String query) {
         RestTemplate rest = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
+
+        //ARC에서 복사 할 수 있는 기본 구
         headers.add("X-Naver-Client-Id", "hR_Jm2it_lWlEK9aHT0L");
         headers.add("X-Naver-Client-Secret", "XiDP4EQcns");
         String body = "";
 
         HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
-        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=Iphone", HttpMethod.GET, requestEntity, String.class);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=" + query, HttpMethod.GET, requestEntity, String.class);
         HttpStatus httpStatus = responseEntity.getStatusCode();
         int status = httpStatus.value();
         String response = responseEntity.getBody();
@@ -25,8 +35,18 @@ public class NaverShopSearch {
         return response;
     }
 
-    public static void main(String[] args) {
-        NaverShopSearch naverShopSearch = new NaverShopSearch();
-        naverShopSearch.search();
+    //JSON을 Item으로 받아온다.
+    public List<ItemDto> fromJSONtoItems(String result){
+        JSONObject rjson = new JSONObject(result);
+        JSONArray items = rjson.getJSONArray("items");
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i=0; i<items.length(); i++) {
+            JSONObject itemJson = (JSONObject) items.get(i);
+            ItemDto itemDto = new ItemDto(itemJson);
+            itemDtoList.add(itemDto);
+        }
+        return itemDtoList;
     }
 }
